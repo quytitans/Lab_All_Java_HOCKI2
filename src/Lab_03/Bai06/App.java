8468c4830f7badf61984c58f05f7e6a86e218a23
quytitans <78024669+quytitans@users.noreply.github.com>
2021-06-18 23:09:06 +0700

    aa

---
+++ src/Lab_03/Bai06/App.java
@@ -0,0 +1,9 @@
+package Lab_03.Bai06;
+
+public class App {
+    public static void main(String args[]){
+        for (String argument:args){
+            System.out.println(argument);
+        }
+    }
+}
